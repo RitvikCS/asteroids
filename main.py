ae0b573bfc0a167e462b5619b8096037be6f5667
Chemudupati Srinivas Ritvik <csritvik@outlook.com>
2024-12-28 13:48:27 +0530

    player render

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -8,13 +9,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     i = 0
+    timer = pygame.time.Clock()
+    dt = 0
     while i == 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = timer.tick(60) / 1000
 
 
 if __name__ == "__main__":
